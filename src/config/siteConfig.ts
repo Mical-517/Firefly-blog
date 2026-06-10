af55527977524531481369c843836ef6100b020f
Mical <213712469@qq.com>
2026-06-10 18:47:55 +0800

    update 友链

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -209,13 +209,13 @@ export const siteConfig: SiteConfig = {
 		// 51la 统计配置
 		la51Analytics: {
 			// 51la 统计 ID
-			Id: "3145333",
+			Id: "3QCx0JDthVzG1E9g",
 			// 自定义 SDK JS 地址，防止 DNS 污染，留空使用默认地址
 			sdkUrl: "",
 			// 多个统计 ID 的数据分离标识，留空则使用 Id
-			ck: "",
+			ck: "3QCx0JDthVzG1E9g",
 			// 是否开启事件分析功能
-			autoTrack: true,
+			autoTrack: false,
 			//  Hash路由模式, 项目使用History API路由, 所以不必开启默认false
 			hashMode: false,
 			// 是否开启网站录屏功能
